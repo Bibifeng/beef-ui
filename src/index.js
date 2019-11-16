b7bb47894f0648dc1c285c16b8fe5cc34e954591
Bibifeng <13714227026@163.com>
2019-11-16 23:11:15 +0800

    style:代码格式化

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const install = function (Vue, opts = {}) {
   components.forEach(component => {
     Vue.component(component.name, component);
   });
-};
+}
 
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue);
